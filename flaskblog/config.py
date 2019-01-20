b850082b7ed70cd9f398a8ea3181d00c49399265
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2019-01-21 01:57:38 +0600

    Add files via upload

---
+++ flaskblog/config.py
@@ -0,0 +1,9 @@
+
+class Config:
+	SECRET_KEY = '2a9c7cfdf6f2554192e6f228e9d772b4'
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+	MAIL_SERVER ='smtp.googlemail.com'
+	MAIL_PORT = 587
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = ''
+	MAIL_PASSWORD = ''
